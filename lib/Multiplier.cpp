b4c8061d1af296ce3f5874291193944a884a34b8
HectorHernandezMarques <H.Hernandez@alumnos.upm.es>
2019-11-14 08:14:24 +0100

    Integrate LibAdder 2.0.2.

--- lib/Multiplier.cpp
+++ lib/Multiplier.cpp
@@ -4,6 +4,7 @@
 namespace libmultiplier {
 
     static int multiply(int mul1, int mul2, int accumulator) {
+        using namespace libadder;
         return 1 < mul2 ? multiply(mul1, mul2 - 1, add(accumulator, mul1))
                         : mul2 < -1 ? multiply(mul1, mul2 + 1, add(accumulator, mul1))
                                     : mul2 == 1 ? add(accumulator, mul1)
